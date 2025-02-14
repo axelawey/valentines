9e1687357eba1f07dbde90e929b8f013006b7f82
Axel Awey <124207550+axelawey@users.noreply.github.com>
2025-02-14 13:52:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    alert("Click the card and tap buttons for custom playlists!");
 
     if (window.innerHeight <= 768) {
         $('.love-button').css('height', '70px');
